ea491a2dfe858e25b2bf9590016255b3aaff0dd4
th0liveura <thiago.h.oliv@gmail.com>
2013-09-23 01:21:41 -0300

    Correction array in example

--- example/array_data.php
+++ example/array_data.php
@@ -1,27 +1,27 @@
 <?php
 return array(
         array (
-                'label' => 'Nome Sala',
+                'label' => 'Activitie',
                 'series' => array (
                         array (
-                                'label' => 'Periodo 1',
+                                'label' => 'Sub-Activitie 1',
                                 'allocations' => array (
                                         array (
-                                                'label' => 'Label 1',
+                                                'label' => 'Task 1',
                                                 'start' => '2013-09-15',
                                                 'end' => '2013-09-29',
                                                 'description' => 'Description x',
                                                 'color' => '#00ff00',
                                         ),
                                         array (
-                                                'label' => 'Label 2',
+                                                'label' => 'Task 2',
                                                 'start' => '2013-09-04',
                                                 'end' => '2013-09-06',
                                                 'description' => 'Description y',
                                                 'color' => '#0000ff',
                                         ),
                                         array (
-                                                'label' => 'Label 3 (Example Conflict)',
+                                                'label' => 'Task 3 (Example Conflict)',
                                                 'start' => '2013-09-17',
                                                 'end' => '2013-09-24',
                                                 'description' => 'Description z',
